61e250ea7cd629889ed39e5c95f95aa13a8186a4
BIIICOD <74822452+BIIICOD@users.noreply.github.com>
2023-11-13 23:44:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function isLuckyNumber(number){
 
+  let leftSide = 0, rightSide = 0;
+
+  if (number.length % 2 === 0){
+
+    for (let i = 0; i<number.length/2; i++){
+      leftSide += Number(number[i]);
+    }
+
+    for (let i = number.length/2; i<number.length; i++){
+      rightSide += Number(number[i]);
+    }
+
+    if (leftSide === rightSide){
+      console.log (number + ' Is lucky number');
+    }
+    else console.log (number + ' Is not lucky number');
+  }
+  else console.log('Number not % 2 = 0')
+}
+
+isLuckyNumber('385916');
