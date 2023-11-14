c90891c21252edad55dad78b967645246312233b
BIIICOD <74822452+BIIICOD@users.noreply.github.com>
2023-11-14 12:53:01 +0300

    DZ-4

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const fib = (pos) => {
+  let firstNumber = 0;
+  let secondNumber = 1;
+  let cashe = 0;
 
+  for (let i = 0; i<pos; i++){
+    cashe = firstNumber;
+    firstNumber = firstNumber + secondNumber;
+    secondNumber = cashe;
+  }
+  console.log(firstNumber);
+}
+
+fib(10);
