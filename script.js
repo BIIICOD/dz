ba2da23c9ea07aa523acf532c9f75c45e828fa8c
BIIICOD <74822452+BIIICOD@users.noreply.github.com>
2023-11-14 11:26:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function invertCase(text){
+  let newString = '';
+  for (let symb of text){
+    symb.toLowerCase() === symb ? newString += symb.toUpperCase() : newString += symb.toLowerCase();
+  }
+  console.log(newString);
+}
 
+invertCase("Hello World");
