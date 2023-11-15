cec421a7064d9691ffa86791ee6448d1f3588ab7
BIIICOD <74822452+BIIICOD@users.noreply.github.com>
2023-11-15 13:21:27 +0300

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,11 +1,1 @@
-export default function cloneDeep(obj) {
-    let clone = {};
-    for (let prop in obj) {
-      clone[prop] = obj[prop];
-      if (typeof clone[prop] === "object") {
-        cloneDeep(clone[prop]);
-      }
-    }
-    return clone;
-  }
-  
+
