e53367949b95062106113498cec3d530692c2b8d
BIIICOD <74822452+BIIICOD@users.noreply.github.com>
2023-11-15 17:23:45 +0300

    Update dnaToRna.js

--- dnaToRna.js
+++ dnaToRna.js
@@ -1,26 +1,46 @@
+// export default function dnaToRna(dna) {
+//   let rna = "";
+//   for (let i = 0; i < dna.length; i++) {
+//     //console.log(dna.at(i) === "G");
+//     switch (dna.at(i)) {
+//       case "G":
+//         rna += "C";
+//         break;
+//       case "C":
+//         rna += "G";
+//         break;
+//       case "T":
+//         rna += "A";
+//         break;
+//       case "A":
+//         rna += "U";
+//         break;
+//       case "":
+//         return console.log("");
+//       default:
+//         return console.log("null");
+//     }
+//   }
+//   console.log(rna);
+// }
+
 export default function dnaToRna(dna) {
+  let symb = {
+    C: "G",
+    G: "C",
+    A: "U",
+    T: "A"
+  };
+
   let rna = "";
-  for (let i = 0; i < dna.length; i++) {
-    //console.log(dna.at(i) === "G");
-    switch (dna.at(i)) {
-      case "G":
-        rna += "C";
-        break;
-      case "C":
-        rna += "G";
-        break;
-      case "T":
-        rna += "A";
-        break;
-      case "A":
-        rna += "U";
-        break;
-      case "":
-        return console.log("");
-      default:
-        return console.log("null");
-    }
+
+  if (dna === "") return console.log("");
+
+  for (const iter of dna) {
+    if (symb[iter] === undefined) return console.log("null");
+    else rna += symb[iter];
   }
   console.log(rna);
 }
+
 
