94619d12252be78b8d63c7eae88e249c2c43d8fd
BIIICOD <74822452+BIIICOD@users.noreply.github.com>
2023-11-15 16:43:00 +0300

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -1,1 +1,7 @@
+import fromPairs from './fromPairs.js'
 
+fromPairs([['cat', 5], ['dog', 6], ['cat', 11]]);
+// { 'cat': 11, 'dog': 6 }
+
+fromPairs([['fred', 30], ['barney', 40]]);
+// { 'fred': 30, 'barney': 40 }
